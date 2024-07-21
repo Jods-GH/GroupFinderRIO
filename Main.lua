bf2233e3c7749df6022d5cfae9dfb766ab4e29d4
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2024-07-21 16:13:15 +0200

    change to using ace framework

---
+++ Main.lua
@@ -0,0 +1,59 @@
+local appName, GFIO = ...
+local AceConfig = LibStub("AceConfig-3.0")
+local AceConfigDialog = LibStub("AceConfigDialog-3.0")
+local AceDBOptions = LibStub("AceDBOptions-3.0")
+---@class MyAddon : AceAddon-3.0, AceConsole-3.0, AceConfig-3.0, AceGUI-3.0, AceConfigDialog-3.0
+local GroupFinderRIO = LibStub("AceAddon-3.0"):NewAddon("GroupFinderRIO", "AceConsole-3.0", "AceEvent-3.0")
+
+
+function GroupFinderRIO:OnInitialize()
+	-- Called when the addon is loaded
+    self:Print(GFIO.getLocalisation("AccessOptionsMessage"))
+    self:RegisterEvent("LFG_LIST_APPLICANT_UPDATED")
+    GFIO.self = self
+    GFIO.db = LibStub("AceDB-3.0"):New("GroupFinderRIO",GFIO.OptionDefaults, true) -- Generates Saved Variables with default Values (if they don't already exist)
+
+    local OptionTable = {
+        type = "group",
+        args = {
+            profile = AceDBOptions:GetOptionsTable(GFIO.db),
+            rest = GFIO.options
+    }}
+    AceConfig:RegisterOptionsTable(appName,OptionTable) -- 
+    AceConfigDialog:AddToBlizOptions(appName, appName)
+    self:RegisterChatCommand("gfio", "SlashCommand")
+	self:RegisterChatCommand("GFIO", "SlashCommand")
+end
+
+
+function GroupFinderRIO:OnEnable()
+	GFIO.createOrShowSpecSelectFrame()
+end
+
+function GroupFinderRIO:OnDisable()
+	-- Called when the addon is disabled
+end
+
+function JDT_AddonCompartmentFunction()
+    GroupFinderRIO:SlashCommand("AddonCompartmentFrame")
+end
+
+function GroupFinderRIO:SlashCommand(msg) -- called when slash command is used
+    if msg == "version" then
+        print(GFIO.AddonVersion)
+    elseif msg == "classFrame" then
+        GFIO.createOrShowSpecSelectFrame()
+    else
+        GFIO.CreateOptionsFrame() 
+    end
+end
+local APPLICATION_CANCELED = "cancelled"
+local APPLICATION_TIMEDOUT = "timedout"
+function GroupFinderRIO:LFG_LIST_APPLICANT_UPDATED(event, applicantID)
+    if event == "LFG_LIST_APPLICANT_UPDATED" then
+        local applicantInfo = C_LFGList.GetApplicantInfo(applicantID)
+        if not applicantInfo or applicantInfo.applicationStatus == APPLICATION_CANCELED or applicantInfo.applicationStatus == APPLICATION_TIMEDOUT then
+            C_LFGList.RefreshApplicants()
+        end
+	end
+end
